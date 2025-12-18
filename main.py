6294271a4d8917bf0763ffbcc1bda6f7aeb09234
Ratneshdtdc <146315467+Ratneshdtdc@users.noreply.github.com>
2025-12-18 18:49:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def build_distance_matrix(df_customers, store_lat, store_lon):
                 continue
     
             #d, t = road_distance_time(lat1, lon1, lat2, lon2)
-            d, t = road_distance_time(lat1, lon1, lat2, lon2, SPEED_KMPH)
+            d, t = road_distance_time(lat1, lon1, lat2, lon2)
 
     
             DIST_MATRIX[(id1, id2)] = d
@@ -114,7 +114,7 @@ def get_distance_time(lat1, lon1, lat2, lon2, speed_kmph):
 
 
 @st.cache_data(show_spinner=False)
-def road_distance_time(lat1, lon1, lat2, lon2, speed_kmph):
+def road_distance_time(lat1, lon1, lat2, lon2):
     try:
         url = (
             f"http://router.project-osrm.org/route/v1/driving/"
@@ -130,10 +130,11 @@ def road_distance_time(lat1, lon1, lat2, lon2, speed_kmph):
         return route["distance"] / 1000, route["duration"] / 60
 
     except Exception:
-        # fallback: aerial distance × road factor
-        DEFAULT_SPEED = 15  # km/h (fixed fallback)
+        # SAFE fallback (NO external dependency)
+        DEFAULT_SPEED = 15  # km/h
         d = haversine(lat1, lon1, lat2, lon2) * 1.4
         return d, (d / DEFAULT_SPEED) * 60
+
 
 
 
